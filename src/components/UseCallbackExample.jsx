b239016194687f7fe82611d2d5467a454781e966
Paul <pdibenedetto@users.noreply.github.com>
2022-04-05 16:43:17 -0400

    Use Callback example

--- src/components/UseCallbackExample.jsx
+++ src/components/UseCallbackExample.jsx
@@ -1,9 +1,32 @@
-import React from 'react'
+// Higher Ordered Component
+import React, { useState, useCallback } from 'react'
 
 const UseCallbackExample = () => {
+  const [tasks, setTasks] = useState([])
+
+  const addTask = useCallback(() => {
+    setTasks((prevState) => [...prevState, 'Some Task'])
+  }, [setTasks])
+
   return (
-    <div>UseCallbackExample</div>
+    <div>
+      <Button addTask={addTask} />
+      {tasks.map((task, index) => (
+        <p key={index}>{task}</p>
+      ))}
+    </div>
   )
 }
+
+const Button = React.memo(({ addTask }) => {
+  console.log('Button Rendered')
+  return (
+    <div>
+      <button className='btn btn-primary' onClick={addTask}>
+        Add Task
+      </button>
+    </div>
+  )
+})
 
 export default UseCallbackExample
